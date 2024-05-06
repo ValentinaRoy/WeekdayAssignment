c40a760ff4aede8778f76bef2515ff397cc7b39e
valentina roy <tinar478612@gmail.com>
2024-05-06 17:43:04 +0530

    Created job card and added styling

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import  Cards  from './Components/Cards';
 
 function App() {
+  const [jobs, setJobs] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+  
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const myHeaders = new Headers({
+          "Content-Type": "application/json"
+        });
+
+        const body = JSON.stringify({
+          "limit": 10,
+          "offset": 0
+        });
+
+        const requestOptions = {
+          method: "POST",
+          headers: myHeaders,
+          body: body
+        };
+
+        const response = await fetch('https://api.weekday.technology/adhoc/getSampleJdJSON', requestOptions);
+
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        console.log(data);
+        if (!data.jdList || !Array.isArray(data.jdList)) {
+          throw new Error('Data format error: expected an array of jobs');
+        }
+        setJobs(data.jdList);
+      } catch (error) {
+        console.error('Fetch error:', error);
+        setError(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Cards jobs={jobs}/>
+    </>
   );
 }
 
